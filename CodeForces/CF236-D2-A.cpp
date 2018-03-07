3b886f0d67fd3e5e04a46a77a3619c7081d79b24
mohamed22ahmed <mohammedahmedhammam113@gmail.com>
2018-03-07 23:23:31 +0200

    add

---
+++ CodeForces/CF236-D2-A.cpp
@@ -0,0 +1,17 @@
+#include bitsstdc++.h
+#define  Mo_Ha_Med  stdios_basesync_with_stdio(0),cin.tie(NULL),cout.tie(NULL);
+using namespace std;
+int main()
+{
+    Mo_Ha_Med
+
+    setchara;
+    string s;
+    cins;
+    for(int i=0;is.size();i++)
+        a.insert(s[i]);
+    if(a.size()%2==0)
+        coutCHAT WITH HER!n;
+    else
+        coutIGNORE HIM!n;
+}
