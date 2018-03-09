0be2db8fad6e7dbadf4eee0cfcdc7547ea73daf4
mohamed22ahmed <36954693+mohamed22ahmed@users.noreply.github.com>
2018-03-09 18:24:11 +0200

    Add files via upload

---
+++ UVA/U,V,A_10815.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+#define  Mo_Ha_Med  std::ios_base::sync_with_stdio(0),cin.tie(NULL),cout.tie(NULL);
+typedef long long ll;
+using namespace std;
+void small(string &n)
+{
+	for(int i=0;i<n.size();i++)
+		if(n[i]>='A'&&n[i]<='Z')
+			n[i]+=32;
+}
+int main()
+{
+    Mo_Ha_Med
+
+
+    set<string>a;
+    set<string>::iterator it;
+    string x,s;
+    while(getline(cin,s))
+    {
+        x="";
+        small(s);
+        for(int i=0;i<s.size();i++)
+            if(s[i]>='a'&&s[i]<='z')
+                x+=s[i];
+            else
+                {
+                    if(!x.empty())
+                        a.insert(x);
+                    x="";
+                }
+        if(!x.empty())
+            a.insert(x);
+    }
+    for(it=a.begin();it!=a.end();it++)
+        cout<<*it<<endl;
+}
